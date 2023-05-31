69f52b0ea97695e28555dcab9dc00bf9712c103b
Niteen01 <kumarniteen063@gmail.com>
2023-05-31 17:53:05 +0530

    tried fetching daat from API and showing it on screen

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-const dropList = document.querySelectorAll(".drop-list select");
+const dropList = document.querySelectorAll(".drop-list select"),
+getbtn=document.querySelector("form"),
+fromcurr=document.querySelector(".from select"),
+tocurr=document.querySelector(".to select");
+
 
 for (let i = 0; i < dropList.length; i++) {
     for(let currency_code in country_code){
-        console.log(currency_code)
        
-        let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+        let selected;
+        if (i == 0){
+            selected = currency_code == "USD" ? "selected" : "";
+        }
+        else if(i == 1){
+            selected = currency_code == "INR"? "selected" : "";
+        }
+       
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
 // 
     }
 }
 
+getbtn.addEventListener("click" , e=>{
+        e.preventDefault(); //prevent form from submitting
+        getExchangeRate();
+});
+function getExchangeRate(){
+    const amount = document.querySelector(".form input");
+    let amountVal = amount.value;
+
+    if(amountVal == 0 || amountVal == ""){
+        amount.value= "1";
+        amountVal = 1;
+    }
+    let url = `https://v6.exchangerate-api.com/v6/e3ce425802e4dbe9b7bcf309/latest/${fromcurr.value}`;
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[tocurr.value];
+        let finalamount = (amountVal * exchangeRate).toFixed(2);
+        console.log(finalamount);
+        exchangeRate = document.querySelector(".outcome");
+        exchangeRate.innerTExt= `${amountVal} ${fromcurr.value} = ${finalamount} ${tocurr.value}`;
+    })
+}
